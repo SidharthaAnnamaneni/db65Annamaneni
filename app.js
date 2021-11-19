a9ceaacd9195c1418a6849a0e8f1c8b2ef5c5fee
Sidhartha Annamaneni <S545265@nwmissouri.edu>
2021-11-18 22:34:01 -0600

    Update app.js

--- app.js
+++ app.js
@@ -70,7 +70,7 @@ var Account =require('./models/account');
  
 passport.use(new LocalStrategy(Account.authenticate())); 
 passport.serializeUser(Account.serializeUser()); 
-passport.deserializeUser(Account.deserializeUser());
+passport.deserializeUser(Account.deserializeUser()) ;
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));

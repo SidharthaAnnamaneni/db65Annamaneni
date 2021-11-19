f658dbc0b2fd29194a40a21bc028167548345a19
Sidhartha Annamaneni <S545265@nwmissouri.edu>
2021-11-18 22:06:03 -0600

    c

--- controllers/kites.js
+++ controllers/kites.js
@@ -118,3 +118,30 @@ exports.costume_create_Page =async  function(req, res) {
         res.send(`{'error': '${err}'}`); 
     } 
 }; 
+// Handle building the view for updating a costume. 
+// query provides the id 
+exports.kites_update_Page =  async function(req, res) { 
+    console.log("update view for item "+req.query.id) 
+    try{ 
+        let result = await kites.findById(req.query.id) 
+        res.render('kitesupdate', { title: 'Kite Update', toShow: result }); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
+}; 
+// Handle a delete one view with id from query 
+exports.kites_delete_Page = async function(req, res) { 
+    console.log("Delete view for id "  + req.query.id) 
+    try{ 
+        result = await kites.findById(req.query.id) 
+        res.render('kitesdelete', { title: 'Kite Delete', toShow: 
+result }); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
+}; 
+ 
--- routes/kites.js
+++ routes/kites.js
@@ -7,5 +7,9 @@ router.get('/', kites_controlers.kites_view_all_Page );
 module.exports = router; 
 /* GET detail kites page */ 
 router.get('/detail', kites_controlers.kites_view_one_Page); 
-/* GET create kites page */ 
-router.get('/create', kites_controlers.kites_create_Page); 
+/* GET create  page */ 
+router.get('/create', kites_controlers.kites_create_post); 
+/* GET create update page */ 
+router.get('/update', kites_controlers.kites_update_Page); 
+/* GET create costume page */ 
+router.get('/delete', kites_controlers.kites_delete_Page); 
